b39eff05c7f190229b0d799af2e630d1d7d37e68
Doğukan Erdoğan <erdoganndogukann@gmail.com>
2026-06-20 00:39:27 +0300

    chore: stop tracking deneme.py

--- deneme.py
+++
@@ -1,8 +0,0 @@
-from aeroquery.detect import Detector
-
-detector = Detector(r"models/best.pt")
-sonuclar = detector.predict(r"models\0000074_09738_d_0000019.jpg")
-
-print(f"{len(sonuclar)} tespit bulundu\n")
-for d in sonuclar[:5]:
-    print(d)
